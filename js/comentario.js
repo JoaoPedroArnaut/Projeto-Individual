aed708427ea41a0c369bc6e95dfbe81572b285a0
JoaoPedroArnaut <joao.silva@bandtec.com.br>
2020-11-19 10:01:01 -0300

    tela nova

---
+++ js/comentario.js
@@ -0,0 +1,24 @@
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    montaMsg(form);
+});
+
+function montaMsg(form){
+    var divContainer = document.createElement('div');
+    divContainer.classList.add('container');
+    var divComentario = document.createElement('div');
+    divComentario.classList.add('comentario');
+    var imgComentario = document.createElement('img');
+    var divBalaoComentario = document.createElement('div');
+    divBalaoComentario.classList.add('balaoComentario')
+
+    var main = document.querySelector('main');
+    main.appendChild(divContainer);
+    divContainer.appendChild(divComentario);
+    divComentario.appendChild(imgComentario);
+    divComentario.appendChild(divBalaoComentario);
+    divBalaoComentario.textContent = form.msg.value;
+    imgComentario.src = form.img.src;
+}
