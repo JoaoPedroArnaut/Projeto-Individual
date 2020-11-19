8ae74408738992c96d4a3303e8ee34a95f6e6890
JoaoPedroArnaut <joao.silva@bandtec.com.br>
2020-11-19 15:56:20 -0300

    melhoria no codigo

--- js/episodios.js
+++ js/episodios.js
@@ -1,41 +1,36 @@
-var request = new XMLHttpRequest();
-
-request.open('GET', 'https://api.jikan.moe/v3/anime/21/episodes/1');
-
-request.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    console.log('Status:', this.status);
-    console.log('Headers:', this.getAllResponseHeaders());
-    console.log('Body:', this.responseText);
-    var converte = JSON.parse(request.responseText);
-    var episodios = converte.episodes;
-    episodios.forEach(episodio => {
-      montaTabela(episodio)
-    });
-  }
-};
-
-request.send();
+var requisicoes = [];
+for (var i = 1; i <= 10; i++) {
+   var requisicao = fetch(`https://api.jikan.moe/v3/anime/21/episodes/${i}`);
+   requisicoes.push(requisicao);
+}
 
 
-var request2 = new XMLHttpRequest();
+var episodios = []
+Promise.all(requisicoes)
+.then(function (resposta) {
+  return resposta[0].json();
+}).then(function (dados) {
+  console.log(dados);
+  var episodios = dados.episodes;
+  episodios.forEach(episodio => {
+    montaTabela(episodio)
+  })
+})
 
-request2.open('GET', 'https://api.jikan.moe/v3/anime/21/episodes/2');
 
-request2.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    console.log('Status:', this.status);
-    console.log('Headers:', this.getAllResponseHeaders());
-    console.log('Body:', this.responseText);
-    var converte = JSON.parse(request2.responseText);
-    var episodios = converte.episodes;
-    episodios.forEach(episodio => {
-      montaTabela(episodio);
-    });
-  }
-};
 
-request2.send();
+// request.onreadystatechange = function () {
+//   if (this.readyState === 4) {
+//     console.log('Status:', this.status);
+//     console.log('Headers:', this.getAllResponseHeaders());
+//     console.log('Body:', this.responseText);
+//     var converte = JSON.parse(request.responseText);
+//     var episodios = converte.episodes;
+//     episodios.forEach(episodio => {
+//       montaTabela(episodio)
+//     });
+//   }
+// };
 
 
 function montaTabela(episodes) {
@@ -46,7 +41,7 @@ function montaTabela(episodes) {
 }
 
 
-function montaTd(dado,classe) {
+function montaTd(dado, classe) {
   var td = document.createElement('td');
   td.textContent = dado;
   td.classList.add(classe);
@@ -56,9 +51,9 @@ function montaTd(dado,classe) {
 function montaTr(episodes) {
   var episodesTr = document.createElement('tr');
 
-  episodesTr.appendChild(montaTd(episodes.episode_id,'numeroEp'));
-  episodesTr.appendChild(montaTd(episodes.title,'nada'));
-  episodesTr.appendChild(montaTd(fillers(episodes.filler),fillers(episodes.filler)));
+  episodesTr.appendChild(montaTd(episodes.episode_id, 'numeroEp'));
+  episodesTr.appendChild(montaTd(episodes.title, 'nada'));
+  episodesTr.appendChild(montaTd(fillers(episodes.filler), fillers(episodes.filler)));
 
   return episodesTr;
 }
